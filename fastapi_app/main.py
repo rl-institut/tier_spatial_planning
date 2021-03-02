e0162e694e984c7ed5780e3f3926e43e83465460
francois.nzale <francois.nzale@gmail.com>
2021-03-02 18:23:14 +0100

    fix issues with coordinates of links

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,14 +1,15 @@
 from fastapi.param_functions import Query
-import models
 from fastapi import FastAPI, Request, Depends, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from database import SessionLocal, engine
-from pydantic import BaseModel
-from models import Nodes
+import models
+from models import Nodes, Links, AddNodeRequest, OptimizeGridRequest
 from sqlalchemy.orm import Session
-from typing import Optional
 import sqlite3
+from sgdot.grids import Grid
+from sgdot.tools.grid_optimizer import GridOptimizer
+import math
 
 
 app = FastAPI()
@@ -18,20 +19,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 models.Base.metadata.create_all(bind=engine)
 
 templates = Jinja2Templates(directory="templates")
-
-
-class AddNodeRequest(BaseModel):
-    latitude: float
-    longitude: float
-    node_type: str
-    fixed_type: bool
-
-
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
 
 
 def get_db():
@@ -57,6 +44,13 @@ async def get_nodes(request: Request, db: Session = Depends(get_db)):
     return result
 
 
+@app.get("/links_db_html")
+async def get_links(request: Request, db: Session = Depends(get_db)):
+    res = db.execute("select * from links")
+    result = res.fetchall()
+    return result
+
+
 def compute_links():
     pass
 
@@ -75,7 +69,7 @@ async def add_node(add_node_request: AddNodeRequest,
     db.add(nodes)
     db.commit()
 
-    background_tasks.add_task(compute_links)
+    # background_tasks.add_task(compute_links)
 
     return {
         "code": "success",
@@ -83,13 +77,124 @@ async def add_node(add_node_request: AddNodeRequest,
     }
 
 
-@app.post("/clear_node_db/")
-async def add_node():
+@app.post("/optimize_grid/")
+async def optimize_grid(optimize_grid_request: OptimizeGridRequest,
+                        background_tasks: BackgroundTasks,
+                        db: Session = Depends(get_db)):
+    res = db.execute("select * from nodes")
+    nodes = res.fetchall()
+
+    grid = Grid(price_meterhub=optimize_grid_request.price_meterhub,
+                price_household=optimize_grid_request.price_household,
+                price_interhub_cable_per_meter=optimize_grid_request.price_interhub_cable,
+                price_distribution_cable_per_meter=optimize_grid_request.price_distribution_cable)
+    opt = GridOptimizer(sa_runtime=20)
+    r = 6371000     # Radius of the earth [m]
+    # use latitude of the node that is the most west to set origin of x coordinates
+    latitude_0 = math.radians(min([node[1] for node in nodes]))
+    # use latitude of the node that is the most south to set origin of y coordinates
+    longitude_0 = math.radians(min([node[2] for node in nodes]))
+    for node in nodes:
+        latitude = math.radians(node[1])
+        longitude = math.radians(node[2])
+
+        x = r * (longitude - longitude_0) * math.cos(latitude_0)
+        y = r * (latitude - latitude_0)
+
+        longitude_recomputed = longitude_0 + x / (r * math.cos(latitude_0))
+        latitude_recomputed = latitude_0 + y / r
+
+        grid.add_node(label=str(node[0]),
+                      pixel_x_axis=x,
+                      pixel_y_axis=y,
+                      node_type=str(node[3]),
+                      type_fixed=bool(node[4]))
+    number_of_hubs = opt.get_expected_hub_number_from_k_means(grid=grid)
+    opt.nr_optimization(grid=grid, number_of_hubs=number_of_hubs, number_of_relaxation_step=10,
+                        save_output=False, save_opt_video=False, plot_price_evolution=False)
 
     sqliteConnection = sqlite3.connect('nodes.db')
     cursor = sqliteConnection.cursor()
 
+    sql_delete_query = """DELETE from links"""
+    cursor.execute(sql_delete_query)
+    sqliteConnection.commit()
+    cursor.close()
+
+    conn = sqlite3.connect('nodes.db')
+    cursor = conn.cursor()
+    records = []
+    count = 1
+    for index, row in grid.get_links().iterrows():
+        x_from = grid.get_nodes().loc[row['from']]['pixel_x_axis']
+        y_from = grid.get_nodes().loc[row['from']]['pixel_y_axis']
+
+        x_to = grid.get_nodes().loc[row['to']]['pixel_x_axis']
+        y_to = grid.get_nodes().loc[row['to']]['pixel_y_axis']
+
+        long_from = math.degrees(longitude_0 + x_from / (r * math.cos(latitude_0)))
+
+        lat_from = math.degrees(latitude_0 + y_from / r)
+
+        long_to = math.degrees(longitude_0 + x_to / (r * math.cos(latitude_0)))
+
+        lat_to = math.degrees(latitude_0 + y_to / r)
+
+        cable_type = row['type']
+        distance = row['distance']
+
+        records.append((count,
+                        lat_from,
+                        long_from,
+                        lat_to,
+                        long_to,
+                        cable_type,
+                        distance))
+        count += 1
+
+    cursor.executemany('INSERT INTO links VALUES(?, ?, ?, ?, ?, ?, ?)', records)
+
+    # commit the changes to db
+    conn.commit()
+    # close the connection
+    conn.close()
+
+    return {
+        "code": "success",
+        "message": "grid optimized"
+    }
+
+
+@ app.post("/clear_node_db/")
+async def clear_nodes():
+    sqliteConnection = sqlite3.connect('nodes.db')
+    cursor = sqliteConnection.cursor()
+
     sql_delete_query = """DELETE from nodes"""
+    cursor.execute(sql_delete_query)
+    sqliteConnection.commit()
+    cursor.close()
+
+    sqliteConnection = sqlite3.connect('nodes.db')
+    cursor = sqliteConnection.cursor()
+
+    sql_delete_query = """DELETE from links"""
+    cursor.execute(sql_delete_query)
+    sqliteConnection.commit()
+    cursor.close()
+
+    return {
+        "code": "success",
+        "message": "nodes cleared"
+    }
+
+
+@ app.post("/clear_link_db/")
+async def clear_links():
+    sqliteConnection = sqlite3.connect('nodes.db')
+    cursor = sqliteConnection.cursor()
+
+    sql_delete_query = """DELETE from links"""
     cursor.execute(sql_delete_query)
     sqliteConnection.commit()
     cursor.close()
--- fastapi_app/static/script.js
+++ fastapi_app/static/script.js
@@ -10,6 +10,7 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       }).addTo(mainMap);
 
 var markers = [];
+var lines = [];
 
 mainMap.on('click', function(e) {
   var poplocation = e.latlng;
@@ -25,7 +26,6 @@ function drawDefaultMarker(latitude, longitude) {
 }
 
 function addNodeToDatBase(latitude, longitude, node_type, fixed_type) {
-
   $.ajax({
     url: "add_node",
     type: "POST",
@@ -40,9 +40,21 @@ function addNodeToDatBase(latitude, longitude, node_type, fixed_type) {
   });
 }
 
-function updateMarkers() {
-  
+function optimize_grid(price_meterhub, price_household, price_interhub_cable, price_distribution_cable) {
+  $.ajax({
+    url: "optimize_grid",
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify({
+      price_meterhub: price_meterhub,
+      price_household: price_household,
+      price_interhub_cable: price_interhub_cable,
+      price_distribution_cable: price_distribution_cable,
+    }),
+    dataType: "json",
+  });
 }
+
 
 function refreshNodeTable() {
     var tbody_nodes = document.getElementById("tbody_nodes");
@@ -74,15 +86,90 @@ function refreshNodeTable() {
         for (node of nodes) {
           markers.push(L.marker([node.latitude, node.longitude]).addTo(mainMap))
         }
+        
       }
     };
   }
 
+function drawLinkOnMap(latitude_from,
+                       longitude_from,
+                       latitude_to,
+                       longitude_to,
+                       color,
+                       map,
+                       weight=3,
+                       opacity=0.5) {
+  var pointA = new L.LatLng(latitude_from, longitude_from);
+  var pointB = new L.LatLng(latitude_to, longitude_to);
+  var pointList = [pointA, pointB];
+
+  var link_polyline = new L.Polyline(pointList, {
+      color: color,
+      weight: weight,
+      opacity: 0.5,
+      smoothFactor: 1
+    });
+  lines.push(
+    link_polyline.addTo(map)
+  );
+}
+
+function ereaseLinksFromMap(map) {
+  for (line of lines){
+    map.removeLayer(line);
+  }
+  lines.length = 0;
+}
+
+function refreshLinkTable() {
+    var tbody_links = document.getElementById("tbody_links");
+    var xhr = new XMLHttpRequest();
+    url = "links_db_html";
+    xhr.open("GET", url, true);
+    xhr.responseType = "json";
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        links = this.response;
+        html_link_table = "";
+        for (link of links) {
+          html_link_table += `
+              <tr>
+              <td>${link.id}</td>
+              <td>${link.lat_from}</td>
+              <td>${link.long_from}</td>
+              <td>${link.lat_to}</td>
+              <td>${link.long_to}</td>
+              <td>${link.cable_type}</td>
+              <td>${link.distance}</td>
+              </tr>`;
+            }
+        tbody_links.innerHTML = html_link_table;
+        ereaseLinksFromMap(mainMap)
+        for (link of links) {
+          var color = (link.cable_type === "interhub") ? "red":"green"
+          drawLinkOnMap(link.lat_from,
+                        link.long_from,
+                        link.lat_to,
+                        link.long_to,
+                        color,
+                        mainMap)
+        }
+        }
+      }
+    };
+  
+
+
+
   $(document).ready(function () {
     refreshNodeTable();
+    refreshLinkTable();
 
-    setInterval(refreshNodeTable, 1000);
-    // setInterval(updateMarkers, 100);
+    setInterval(refreshNodeTable, 3000);
+    setInterval(refreshLinkTable, 3000);
+    
 
     $("#button_add_node").click(function () {
       const latitude = new_node_lat.value;
@@ -91,6 +178,15 @@ function refreshNodeTable() {
       const fixed_type = new_node_type_fixed.value;
 
       addNodeToDatBase(latitude, longitude, node_type, fixed_type)
+      
+    });
+   
+    $("#button_optimize").click(function () {
+      const price_hub = hub_price.value;
+      const price_household = household_price.value;
+      const price_interhub_cable = interhub_cable_price.value;
+      const price_distribution_cable = interhub_cable_price.value;
+      optimize_grid(price_hub, price_household, price_interhub_cable, price_distribution_cable)
       
     });
 
