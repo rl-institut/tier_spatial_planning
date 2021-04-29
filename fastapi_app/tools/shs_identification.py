a443aa59efdd40779933704aab7f7d98d9e867a4
francois.nzale <francois.nzale@gmail.com>
2021-04-29 15:51:25 +0200

    fix bugs in shs_identification function leading to errors, start to discard lonely nodes

--- fastapi_app/tools/shs_identification.py
+++ fastapi_app/tools/shs_identification.py
@@ -301,8 +301,8 @@ def are_nodes_connected(node_a, node_b, links_df):
     """
     for index_link, row_link in links_df.iterrows():
         if ((row_link['node_a'] == node_a and row_link['node_b'] == node_b)
-                    or (row_link['node_a'] == node_b and row_link['node_b'] == node_a)
-                ):
+                or (row_link['node_a'] == node_b and row_link['node_b'] == node_a)
+            ):
             return True
     return False
 
@@ -334,7 +334,31 @@ def neighoring_nodes(node_index, links_df):
     return neighboring_nodes
 
 
-def nodes_on_branch(stam_node, branch_first_nodes, links_df, nodes_in_branch):
+def distance_to_clostest_neighbor(ref_node, links_df):
+    """
+    Returns the index of the clostest neighbor or the node given as parameter.
+
+    Parameters
+    ----------
+    ref_node (str):
+        index of the reference node considered.
+
+    links_df (pandas.DataFrame)
+            Pandas DataFrame containing the links connecting the network. In the form
+                            node_a     node_b  distance
+            label
+            node0, node1    node0  node1    2.2360
+            (node1, node2)  node0  node2    2.8284
+
+    Output
+    ------
+    index of the neighboring node that is the closest to the ref_node given as parameter
+    """
+
+    return links_df[(links_df['node_a'] == ref_node) | (links_df['node_b'] == ref_node)]['distance'].min()
+
+
+def nodes_on_branch(stam_node, branch_first_nodes, links_df, nodes_in_branch, iteration=0, max_iteration=5):
     """
     This function recursively explores the branch of a tree and returns a list
     of all the nodes on that branch.
@@ -365,6 +389,9 @@ def nodes_on_branch(stam_node, branch_first_nodes, links_df, nodes_in_branch):
         List of all the nodes on the branches originating at stam_node and
         pointing toward the nodes in branch_first_nodes.
     """
+    print(f"nodes_on_branch iteration, iteration: ({iteration}/{max_iteration})")
+    if iteration >= max_iteration:
+        return nodes_in_branch
     for branch_node in branch_first_nodes:
         if branch_node not in nodes_in_branch:
             nodes_in_branch.append(branch_node)
@@ -380,12 +407,15 @@ def nodes_on_branch(stam_node, branch_first_nodes, links_df, nodes_in_branch):
         downstream_nodes = [node for node in neighbors if (
             node != stam_node and node not in nodes_in_branch)]
         nodes_in_branch += downstream_nodes
-        res = nodes_on_branch(node, downstream_nodes,
-                              links_df, nodes_in_branch)
+        res = nodes_on_branch(stam_node=node,
+                              branch_first_nodes=downstream_nodes,
+                              links_df=links_df,
+                              nodes_in_branch=nodes_in_branch,
+                              iteration=iteration+1,
+                              max_iteration=max_iteration)
 
     if len(branch_first_nodes) > 0:
         return res
-
     return nodes_in_branch
 
 
@@ -459,8 +489,10 @@ def nodes_and_links_to_discard(nodes_df,
                                cable_price_per_meter,
                                additional_price_for_connection_per_node,
                                shs_characteristics):
-    print("start nodes_and_links_to_discard (v1)")
+    if nodes_df.shape[0] == 0:
+        return []
 
+    print("start nodes_and_links_to_discard (v1)")
     nodes_shs_price = {node: shs_price_of_node(
         node,
         nodes_df=nodes_df,
@@ -472,9 +504,35 @@ def nodes_and_links_to_discard(nodes_df,
     # distance of both nodes it connects
 
     links_subject_to_disconnection = set()
-    nodes_to_be_disconnected = {}
+    links_to_be_disconnected = set()
+    nodes_to_be_disconnected = set()
 
+    # Compute critical distance for each nodes and discard nodes that are
+    # further away to nearest neighbor than own critical distance
+
+    critial_distance_dict = {node: (
+        nodes_shs_price[node] - additional_price_for_connection_per_node
+    ) / cable_price_per_meter for node in nodes_df.index}
+
+    for node in nodes_df.index:
+        links_connecting_node_df = links_df[(
+            links_df['node_a'] == node) | (links_df['node_b'] == node)]
+
+        if distance_to_clostest_neighbor(node, links_df) > critial_distance_dict[node]:
+            nodes_to_be_disconnected.add(node)
+            nodes_df.drop(node)
+
+            for link_to_neighbors in links_connecting_node_df.index:
+                links_to_be_disconnected.add(link_to_neighbors)
+                links_df.drop(link_to_neighbors)
+            print(f"node: {node} is further away to nn than critical distance")
+            print(f"links: {links_connecting_node_df.index}")
+
+    #################
     for link_index, link_row in links_df.iterrows():
+        # The algorithm will explore the cluster A (branch with stam at node_a
+        # pointing outward relative to node_b).
+        # If node_b is a leaf-node, switch node_a & node_b
         node_a = link_row['node_a']
         node_b = link_row['node_b']
 
@@ -486,31 +544,229 @@ def nodes_and_links_to_discard(nodes_df,
             nodes_shs_price[node_b] - additional_price_for_connection_per_node
         ) / cable_price_per_meter
 
-        # TODO Use for loop with zip for node_a and node_b
-        if link_row['distance'] > critial_distance_node_a:
-            links_subject_to_disconnection.add(link_index)
-            if count_number_of_connections(node_index=node_a, links_df=links_df) == 1:
-                # Compute nodes that are on branch a
-                nodes_on_branch_a = nodes_on_branch(
-                    stam_node=node_b,
-                    branch_first_nodes=node_a,
-                    links_df=links_df,
-                    nodes_in_branch=[])
+        # First remove all nodes that are further away than respective critical
+        # distance from nearest neighbor. Also remove related links.
 
-                # Compute price of shs for all nodes in cluster a
-                price_shs_on_branch = sum([nodes_shs_price[node] for node in nodes_on_branch_a])
+        for node in [node_a, node_b]:
+            critial_distance_dict
 
-                # Compute links connecting the nodes of cluster a
-                links_on_branch_a_df = links_df[(links_df['node_a'].isin(nodes_on_branch_a)) | (
-                    links_df['node_b'].isin(nodes_on_branch_a))].index
-                price_links_in_cluster_a = links_df['distance'].sum() * cable_price_per_meter
-                price_connection_in_cluster_a = len(nodes_on_branch_a) * \
-                    additional_price_for_connection_per_node
+            distance_to_closest_neighbor_of_node = distance_to_clostest_neighbor(node, links_df)
 
-                nodes_to_be_disconnected[node_a] = []
+            if ((link_row['distance'] > critial_distance_dict[node])
+                    and (distance_to_closest_neighbor_of_node > critial_distance_dict[node])):
+                links_to_be_disconnected.add(link_index)
+                links_df.drop(link_index)
+                nodes_to_be_disconnected.add(node)
+                nodes_df.drop(node)
+            else:
+                links_subject_to_disconnection.add(link_index)
+
+    #################
+
+    print(
+        f"\n\n####### --->>>{len(links_subject_to_disconnection)}/{links_df.shape[0]} links_subject_to_disconnection: {links_subject_to_disconnection} before starting while loop\n\n\n")
+    if len(links_subject_to_disconnection) == links_df.shape[0]:
+        return list(nodes_df.index)
+    counter = 0
+    max_counter = links_df.shape[0]
+    print(f"max_counter: {max_counter}")
+    while len(links_subject_to_disconnection) > 0 and counter < max_counter:
+        counter += 1
+
+        # Compute link from links_subject_to_disconnection that is the
+        # most favorable to disconnect
+
+        most_favorable_link_to_disconnect = None
+        saving_of_diconnecting_most_favorable_link = - np.infty
+        nodes_disconnected_if_most_favorable_link_is_cutted = []
+
+        print(
+            f"\n\nwhile loop counter {counter}\n----> {nodes_df.shape[0]} nodes in nodes_df: {nodes_df.index}\n----> {links_df.shape[0]} links in links_df: {links_df.index}")
+        print(f"links subjects to disconnection: {links_subject_to_disconnection}")
+
+        links_removed_from_links_subject_to_disconnection = set()
+        for link in links_subject_to_disconnection:
+            if link in links_removed_from_links_subject_to_disconnection:
+                break
+            print(f"\n considering link {link}")
+            # Explore branch a (clsuter containing node_a and all nodes
+            # downstream from a relatve to node_b)
+
+            node_a = link_row['node_a']
+            node_b = link_row['node_b']
+
+            betweenness_centrality_dic = betweenness_centrality(links_df=links_df)
+            betweenness_centrality_a = betweenness_centrality_dic[node_a]
+            betweenness_centrality_b = betweenness_centrality_dic[node_b]
+            if (betweenness_centrality_b < betweenness_centrality_a):
+                print(
+                    f"switched nodes node_a and node_b because betweenness centralities are (a,b)=({betweenness_centrality_a}, {betweenness_centrality_b})")
+                node_a, node_b = node_b, node_a
+
+            # Compute all nodes on branch a
+            nodes_on_branch_a = nodes_on_branch(
+                stam_node=node_b,
+                branch_first_nodes=[node_a],
+                links_df=links_df,
+                nodes_in_branch=[])
+
+            # Compute price of shs for all nodes in branch a
+            price_shs_on_branch_a = sum([nodes_shs_price[node]
+                                         for node in nodes_on_branch_a])
+
+            # Compute links connecting the nodes of branch a ommiting other
+            # links_subject_to_disconnection
+            other_links_subject_to_disconnection = [
+                long_link for long_link in links_subject_to_disconnection
+                if long_link != link]
+            links_on_branch_a_df = links_df[
+                ((links_df['node_a'].isin(nodes_on_branch_a)) | (
+                    links_df['node_b'].isin(nodes_on_branch_a))) &
+                (~links_df.index.isin(other_links_subject_to_disconnection))]
+            price_links_in_branch_a = links_on_branch_a_df['distance'].sum() * cable_price_per_meter
+
+            price_connections_in_branch_a = len(nodes_on_branch_a) * \
+                additional_price_for_connection_per_node
+
+            # Compute saving of disconnecting branch a
+            saving_of_disconecting_branch_a = (
+                price_links_in_branch_a
+                + price_connections_in_branch_a
+                - price_shs_on_branch_a
+            )
+
+            # Compute saving of disconnecting branch b
+            nodes_on_branch_b = [node for node in nodes_df.index
+                                 if node not in nodes_on_branch_a]
+
+            # Compute price of shs for all nodes in branch b
+            price_shs_on_branch_b = sum([nodes_shs_price[node]
+                                         for node in nodes_on_branch_b])
+
+            # Compute links connecting the nodes of branch b
+            links_on_branch_b_df = links_df[
+                ((links_df['node_a'].isin(nodes_on_branch_b)) | (
+                    links_df['node_b'].isin(nodes_on_branch_b))) &
+                (~links_df.index.isin(other_links_subject_to_disconnection))]
+
+            price_links_in_branch_b = links_on_branch_b_df['distance'].sum(
+            ) * cable_price_per_meter
+
+            price_connections_in_branch_b = len(nodes_on_branch_b) * \
+                additional_price_for_connection_per_node
+
+            saving_of_disconecting_branch_b = (
+                price_links_in_branch_b
+                + price_connections_in_branch_b
+                - price_shs_on_branch_b
+            )
+            print(
+                f"saving_if branch A of {link} is disconnected: {saving_of_disconecting_branch_a}")
 
             print(
-                f"link: {link_index} distance {(link_row['distance'])} is larger than critial distances (a, b): {(critial_distance_node_a, critial_distance_node_b)}")
-    print(f"nodes to be disconnected: {nodes_to_be_disconnected}")
-    print(f"links_subject_to_disconnection: {links_subject_to_disconnection}")
+                f"saving_if branch B of {link} is disconnected: {saving_of_disconecting_branch_b}")
+
+            # Identifiy most favorable link to disconnect and nodes that
+            # would thus be disconnected
+            loop_entered = []
+            if (saving_of_disconecting_branch_a
+                    > saving_of_diconnecting_most_favorable_link):
+
+                loop_entered.append("a")
+                saving_of_diconnecting_most_favorable_link =\
+                    saving_of_disconecting_branch_a
+                most_favorable_link_to_disconnect = link
+                print(f"links on branch A to disconnect: {links_on_branch_a_df.index}")
+                nodes_disconnected_if_most_favorable_link_is_cutted =\
+                    nodes_on_branch_a
+
+            if (saving_of_disconecting_branch_b
+                    > saving_of_diconnecting_most_favorable_link):
+                loop_entered.append("b")
+                saving_of_diconnecting_most_favorable_link =\
+                    saving_of_disconecting_branch_b
+                most_favorable_link_to_disconnect = link
+                print(f"links on branch B to disconnect: {links_on_branch_b_df.index}")
+                nodes_disconnected_if_most_favorable_link_is_cutted =\
+                    nodes_on_branch_b
+            if max(saving_of_disconecting_branch_a, saving_of_disconecting_branch_b) < 0:
+                links_removed_from_links_subject_to_disconnection.add(link)
+            print(f"loop_entered: {loop_entered}")
+            print(f"nodes on branch a: {nodes_on_branch_a}")
+            print(f"nodes on branch b: {nodes_on_branch_b}")
+            print(
+                f"   loop -> nodes_disconnected_if_most_favorable_link_is_cutted: {nodes_disconnected_if_most_favorable_link_is_cutted}")
+        if saving_of_diconnecting_most_favorable_link > 0:
+            print(
+                f"    saving_of_diconnecting_most_favorable_link for {most_favorable_link_to_disconnect} is {saving_of_diconnecting_most_favorable_link} > 0")
+            # Remove most favorable link to disconnect from links_df as well as
+            # all links on that branch and the nodes from nodes_df
+            links_to_be_disconnected.add(
+                most_favorable_link_to_disconnect)
+            links_on_disconnected_branch = list(
+                links_df[
+                    (links_df['node_a'].isin(nodes_disconnected_if_most_favorable_link_is_cutted))
+                    & (links_df['node_b'].isin(nodes_disconnected_if_most_favorable_link_is_cutted))
+                ].index
+            )
+            print(f"links_on_disconnected_branch: {links_on_disconnected_branch}")
+
+            links_df = links_df.drop(most_favorable_link_to_disconnect)
+
+            links_to_be_disconnected = links_to_be_disconnected.union(links_on_disconnected_branch)
+            print(f"small links to disconnect: {links_on_disconnected_branch}")
+            for link_to_disconnect in links_on_disconnected_branch:
+                print(f"    drop link {link_to_disconnect} from nodes_df")
+                links_df = links_df.drop(link_to_disconnect)
+            nodes_to_be_disconnected = nodes_to_be_disconnected.union(
+                nodes_disconnected_if_most_favorable_link_is_cutted)
+            for node in nodes_disconnected_if_most_favorable_link_is_cutted:
+                print(f"    drop node {node} from nodes_df")
+                nodes_df = nodes_df.drop(node)
+            print(f"       remaining {links_df.shape[0]} links: {links_df.index}")
+            print(f"       remaining {nodes_df.shape[0]} nodes: {nodes_df.index}")
+            for link_on_disconnected_branch in links_on_disconnected_branch:
+                links_subject_to_disconnection.discard(link_on_disconnected_branch)
+        else:
+            print(f"link: {most_favorable_link_to_disconnect} is not worth disconnecting")
+            print(f"return: {nodes_to_be_disconnected}")
+            return nodes_to_be_disconnected
+
+        for link_retained in links_removed_from_links_subject_to_disconnection:
+            links_subject_to_disconnection.discard(link_retained)
+        # Remove most favorable links from links_subject_to_disconnection
+        links_subject_to_disconnection.discard(most_favorable_link_to_disconnect)
+        print(f"updated links_subject_to_disconnection: {links_subject_to_disconnection}\n\n\n")
+
+        print(f"counter: {counter}")
+        print(f"most favorable link to disconnect: {most_favorable_link_to_disconnect}")
+        print(f"associated nodes: {nodes_disconnected_if_most_favorable_link_is_cutted}")
+        print(
+            f"\n\nend of while loop counter {counter}\n----> {nodes_df.shape[0]} nodes in nodes_df: {nodes_df.index}\n----> {links_df.shape[0]} links in links_df: {links_df.index}\n\n\n")
+
+    print(f"return: {nodes_to_be_disconnected}")
     return nodes_to_be_disconnected
+
+
+def betweenness_centrality(links_df):
+    """
+    This method returns the betweeness centrality of the nodes composing the
+    network.
+    It relies on the networkx.betweenness_centrality of the networkx module.
+    Parameters
+    ----------
+        links_df (pandas.DataFrame)
+            Pandas DataFrame containing the links connecting the network. In the form
+                            node_a   node_b  distance
+            label                                 
+            node0, node1    node0  node1    2.2360
+            (node1, node2)  node0  node2    2.8284
+    Output
+    ------
+        Dictionnary with nodes indices as key and respective betweenness
+        centrality measure.
+    """
+    graph = nx.Graph()
+    for link_index, link_row in links_df.iterrows():
+        graph.add_edge(link_row['node_a'], link_row['node_b'])
+    return nx.betweenness_centrality(graph)
