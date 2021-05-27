4ec09fc16ecf7eb990f2aadff64c8503ce060832
francois.nzale <francois.nzale@gmail.com>
2021-05-27 08:38:49 +0200

    transfer modules grids.py, grid_optimizer.py from sgdotlite package as well as functions from io.py

--- fastapi_app/tools/io.py
+++ fastapi_app/tools/io.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import os
 
 
 def create_empty_nodes_df():
@@ -50,3 +51,23 @@ def create_empty_links_df():
             pd.Series([], dtype=np.dtype(float)),
         }
     ).set_index('label')
+
+
+def make_folder(folder):
+    """
+    If no folder of the given name already exists, create new one.
+
+    Parameters
+    ----------
+    folder: str
+        Name of the folder to be created.
+    """
+
+    if not os.path.exists(folder):
+        parent_folders = folder.split('/')
+        for i in range(1, len(parent_folders) + 1):
+            path = ''
+            for x in parent_folders[0:i]:
+                path += x + '/'
+            if not os.path.exists(path[0:-1]):
+                os.mkdir(path[0:-1])
