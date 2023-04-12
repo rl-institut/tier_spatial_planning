8301d6f8c276bec3cd00f11339af1afb4b9688f4
hacra <hacra@pm.me>
2023-04-12 08:58:39 +0200

    ackee analytics added

--- fastapi_app/db/database.py
+++ fastapi_app/db/database.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
@@ -6,6 +7,8 @@ from fastapi_app.db.config import db_host, db_name, db_user_name, PW, db_port
 from fastapi_app.db.models import Base
 from sqlalchemy.exc import SQLAlchemyError
 from mysql.connector import DatabaseError, ProgrammingError, InterfaceError
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
 
 
 BASE_URL = 'mysql+package://{}:{}@{}:{}/{}'.format(db_user_name, PW, db_host, db_port, db_name)
@@ -22,6 +25,23 @@ for i in range(400):
         async_sessionmaker = scoped_session(sessionmaker(bind=async_engine,
                                                          class_=AsyncSession))
     except (SQLAlchemyError, DatabaseError, ProgrammingError, InterfaceError) as e:
+        time.sleep(5)
+    else:
+        break
+
+
+for i in range(4):
+    try:
+        client = MongoClient('mongodb://localhost:27017')
+        db = client['ackee']
+        domains_collection = db['domains']
+        document = {'title': 'peoplesun.energietechnik.tu-berlin.de',
+                  'id': 'e6b4dbf9-7401-4172-8212-f6a13cd5f962',
+                  'created': datetime.datetime.now(),
+                  'updated': datetime.datetime.now()}
+        query_filter = {"id": document["id"]}
+        result = domains_collection.update_one(query_filter, {"$setOnInsert": document}, upsert=True)
+    except PyMongoError as e:
         time.sleep(5)
     else:
         break
