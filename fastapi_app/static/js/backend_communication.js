b2ac34ebb6ab03b6730b5d04769c8d41a171ecd7
hacra <hacra@pm.me>
2023-04-15 17:38:19 +0200

    restructuring js code

--- fastapi_app/static/js/backend_communication.js
+++ fastapi_app/static/js/backend_communication.js
@@ -102,33 +102,7 @@ function remove_buildings_inside_boundary(
         put_markers_on_map(map_elements, true)})}
 
 
-/************************************************************/
-/*                ACTIVATION / INACTIVATION                 */
-/************************************************************/
-function activation_check() {
-    if (document.getElementById('selectionFile').checked) {
-        document.getElementById('fileImport').disabled = false
-        document.getElementById('btnImport').classList.remove('disabled');
-        document.getElementById('lblDrawBoundariesAdd').classList.add('disabled');
-        document.getElementById('btnDrawBoundariesAdd').classList.add('disabled');
-        document.getElementById('lblDrawBoundariesRemove').classList.add('disabled');
-        document.getElementById('btnDrawBoundariesRemove').classList.add('disabled');
-    } else if (document.getElementById('selectionBoundaries').checked) {
-        document.getElementById('fileImport').disabled = true
-        document.getElementById('btnImport').classList.add('disabled');
-        document.getElementById('lblDrawBoundariesAdd').classList.remove('disabled');
-        document.getElementById('btnDrawBoundariesAdd').classList.remove('disabled');
-        document.getElementById('lblDrawBoundariesRemove').classList.remove('disabled');
-        document.getElementById('btnDrawBoundariesRemove').classList.remove('disabled');
-    } else if (document.getElementById('selectionMap').checked) {
-        document.getElementById('fileImport').disabled = true
-        document.getElementById('btnImport').classList.add('disabled');
-        document.getElementById('lblDrawBoundariesAdd').classList.add('disabled');
-        document.getElementById('btnDrawBoundariesAdd').classList.add('disabled');
-        document.getElementById('lblDrawBoundariesRemove').classList.add('disabled');
-        document.getElementById('btnDrawBoundariesRemove').classList.add('disabled');
-    }
-}
+
 
 function enable_disable_shs() {
     if (document.getElementById('enableShs').checked) {
@@ -845,6 +819,7 @@ function forward_if_consumer_selection_exists(project_id) {
             document.getElementById('section').style.display = 'none';
             document.getElementById('noSelection').style.display='block'
         }})}
+
 
 function hide_no_selection_prompt() {
     document.getElementById('leafletMap').style.display = 'block';
