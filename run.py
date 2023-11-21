abf4dceca0bad76deded8e0e71b0930016a52796
hacra <hacra@pm.me>
2023-11-21 13:33:01 +0100

    ip changed

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from fastapi_app.main import app
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=40000)
-    # go to http://localhost:40000/
+    uvicorn.run(app, host="127.0.0.1", port=40000)
+    # go to http://localhost:127.0.0.1:40000/
