19dffed1a04b7082c4b7b524bb696f91950a946a
hacra <hacra@pm.me>
2023-11-21 17:50:24 +0100

    create db

--- fastapi_app/db/db_con.py
+++ fastapi_app/db/db_con.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from fastapi_app.db.models import Base
 from sqlalchemy.exc import SQLAlchemyError
 from mysql.connector import DatabaseError, ProgrammingError, InterfaceError
-from fastapi_app.db.config import DB_USER_NAME, PW, DB_HOST, DB_PORT, DB_NAME, DOMAIN
+from fastapi_app.db.config import DB_USER_NAME, PW, DB_HOST, DB_PORT, DB_NAME
 
 
 BASE_URL = 'mysql+package://{}:{}@{}:{}/{}'.format(DB_USER_NAME, PW, DB_HOST, DB_PORT, DB_NAME)
@@ -13,9 +13,15 @@ SYNC_DB_URL = BASE_URL.replace('package', 'mysqlconnector')
 ASYNC_DB_URL = BASE_URL.replace('package', 'aiomysql')
 
 
+print(BASE_URL)
+
 for i in range(100):
     try:
         sync_engine = create_engine(SYNC_DB_URL)
+        if DB_USER_NAME == 'root':
+            sync_engine = create_engine(SYNC_DB_URL.replace('/{}'.format(DB_NAME), ''))
+            with sync_engine.connect() as connection:
+                connection.execute("CREATE DATABASE IF NOT EXISTS {}".format(DB_NAME))
         Base.metadata.create_all(bind=sync_engine)
         async_engine = create_async_engine(ASYNC_DB_URL, pool_size=30, max_overflow=150, pool_timeout=30)
     except (SQLAlchemyError, DatabaseError, ProgrammingError, InterfaceError) as e:
