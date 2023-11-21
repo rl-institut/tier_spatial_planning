edf58ed9ae69d56c0bc966621c10a23cc893b83f
hacra <hacra@pm.me>
2023-11-21 16:17:44 +0100

    bug fixed when reading secrets

--- fastapi_app/db/config.py
+++ fastapi_app/db/config.py
@@ -17,28 +17,30 @@ var_list = ['PW',
             'KEY_FOR_ACCESS_TOKEN',
             'EXAMPLE_USER_PW']
 
-if os.environ.get('DOCKERIZED') is None:
+if os.environ.get('DOCKERIZED') is None or bool(os.environ.get('DOCKERIZED')) is False:
     # If the environment variable 'DOCKERIZED' is not set, it is assumed that the app is running outside a docker container.
     # In this case, we need to read the environment files and docker secrets files manually.
     load_dotenv(dotenv_path='fastapi_app.env')
     load_dotenv(dotenv_path='mail.env')
-    root_path = os.getcwd()
-    secret_path = os.path.join(root_path, 'secrets')
-    if not os.path.exists(secret_path):
-        root_path = next((path for path in Path.cwd().parents if path.name == APP_DIR), None)
-        secret_path = os.path.join(root_path, 'secrets') if root_path is not None else None
-    if not os.path.exists(secret_path):
-        raise FileNotFoundError(f"Could not find directory the directory /secrets/")
 
-    file_paths = {'PW': os.path.join(secret_path, 'secret.txt'),
-                  'MAIL_PW': os.path.join(secret_path, 'mail_secret.txt'),
-                  'KEY_FOR_ACCESS_TOKEN': os.path.join(secret_path, 'key_for_token.txt'),
-                  'EXAMPLE_USER_PW': os.path.join(secret_path, 'example_user_secret.txt')}
 
-    for var_name, file_path in file_paths.items():
-        if os.path.exists(file_path):
-            with open(file_path, 'r') as file:
-                os.environ[var_name] = file.read().strip()
+root_path = os.getcwd()
+secret_path = os.path.join(root_path, 'secrets')
+if not os.path.exists(secret_path):
+    root_path = next((path for path in Path.cwd().parents if path.name == APP_DIR), None)
+    secret_path = os.path.join(root_path, 'secrets') if root_path is not None else None
+if not os.path.exists(secret_path):
+    raise FileNotFoundError(f"Could not find directory the directory /secrets/")
+
+file_paths = {'PW': os.path.join(secret_path, 'secret.txt'),
+              'MAIL_PW': os.path.join(secret_path, 'mail_secret.txt'),
+              'KEY_FOR_ACCESS_TOKEN': os.path.join(secret_path, 'key_for_token.txt'),
+              'EXAMPLE_USER_PW': os.path.join(secret_path, 'example_user_secret.txt')}
+
+for var_name, file_path in file_paths.items():
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            os.environ[var_name] = file.read().strip()
 
 def is_set(var_name):
     if os.environ.get(var_name) == 'tbd' or os.environ.get(var_name) is None:
@@ -48,8 +50,8 @@ def is_set(var_name):
 
 for var in mail_vars:
     if is_set(var) is False:
-        print("To set up the mail service, you need to define the environment variable {}.\nWithout the mail"
-              " service, the web app won't be able to send activation links during user registration.".format(var))
+        print("To set up the mail service, you need to define the environment variable \"{}\".\nWithout the mail"
+              " service, the web app won't be able to send activation links during user registration.\n".format(var))
 
 if is_set('PW') is False:
     print("To set up the database, you need to define the environment variable PW")
