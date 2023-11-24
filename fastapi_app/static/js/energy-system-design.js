582af45d0ef089dcf65d702e6df6cd0766340c1c
hacra <hacra@pm.me>
2023-11-24 20:26:58 +0100

    move script to js-files

--- fastapi_app/static/js/energy-system-design.js
+++ fastapi_app/static/js/energy-system-design.js
@@ -1,3 +1,17 @@
+var targetNode = document.getElementById('responseMsg');
+var config = { childList: true, subtree: true, characterData: true };
+var callback = function(mutationsList, observer) {
+    for(let mutation of mutationsList) {
+        if ((mutation.type === 'childList' || mutation.type === 'characterData') && targetNode.textContent.trim() !== '') {
+            var modal = document.getElementById('msgBox');
+            modal.style.display = "block";
+        }
+    }
+};
+var observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
+
+
 var xLeft = 40;
 var yTop = 130;
 var roundCornerBlock = 5;
