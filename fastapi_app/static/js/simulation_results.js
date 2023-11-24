58e154a4547c6be74f1964375c153c9d32d43662
hacra <hacra@pm.me>
2023-11-24 19:47:45 +0100

    move script to js-files

---
+++ fastapi_app/static/js/simulation_results.js
@@ -0,0 +1,16 @@
+document.getElementById('downloadCSV').addEventListener('click', function() {
+window.location.href = '/download_data/{{ project_id }}/csv';
+        });
+var targetNode = document.getElementById('responseMsg');
+var config = { childList: true, subtree: true, characterData: true };
+var callback = function(mutationsList, observer) {
+    for(let mutation of mutationsList) {
+        if ((mutation.type === 'childList' || mutation.type === 'characterData') && targetNode.textContent.trim() !== '') {
+            var modal = document.getElementById('msgBox');
+            modal.style.display = "block";
+        }
+    }
+};
+var observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
+document.getElementById("msgBox").style.zIndex = "9999";
